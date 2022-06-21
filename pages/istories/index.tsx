06c128d47d78b208c72634ebf611ad263658ba52
fdev1.delta4 <fdev1.delta4@gmail.com>
2022-06-21 13:25:16 +0530

    complete

--- pages/istories/index.tsx
+++ pages/istories/index.tsx
@@ -1,53 +1,88 @@
 import Stories, { WithSeeMore } from "react-insta-stories";
 import React, { useState } from "react";
-// import styled from 'styled-components';
 import { HiArrowRight } from 'react-icons/hi'
 import { HiArrowLeft } from 'react-icons/hi'
 import { RiCloseFill } from 'react-icons/ri'
-export default function InstaStories() {
+import { FiPlus } from 'react-icons/fi'
+import { AiFillHeart } from 'react-icons/ai'
 
+
+
+export default function InstaStories() {
     const [currentId, setCurrentId] = useState(0);
+    const [curIndex, setCurIndex] = useState(0);
+    const dataary = data[curIndex].item;
+    function curindex() {
+        const DataLenght = data.length - 1
+        if (curIndex < DataLenght) {
+            setCurIndex(curIndex => curIndex + 1);
+        }
+        else (
+            setCurIndex(0)
+        )
+    }
     return (
         <>
             <section className='StorieSection'>
                 <div className='StorieDev'>
 
                     <div className='close icon'><RiCloseFill /></div>
-                    <div className='left icon'><HiArrowLeft /></div>
+                    {curIndex <= 0 ? <> </> : <div className='left icon' onClick={() => {
+                        setCurIndex(curIndex => curIndex - 1);
+                    }}><HiArrowLeft /></div>}
                     <Stories
                         currentIndex={currentId}
                         loop
                         keyboardNavigation
                         defaultInterval={3000}
-                        stories={stories2}
+                        stories={dataary}
                         onStoryEnd={(s, st) => {
-                            console.log('story ended', s, st);
+                            // console.log('story ended', s, st);
                             setCurrentId(currentId => currentId + 1);
                         }}
                         onAllStoriesEnd={(s, st) => {
-                            console.log('all stories ended', s, st);
+                            // console.log('all stories ended', s, st);
                             setCurrentId(currentId => 0);
+                            curindex()
                         }}
                         onStoryStart={(s, st) => {
-                            console.log('story started', s, st);
-                            setCurrentId(currentId => (currentId + 1) - 1);
+                            // console.log('story started', s, st);
+                            setCurIndex(currentId => (currentId + 1) - 1);
                         }}
                     />
-                    <div className='right icon'><HiArrowRight /></div>
+                    <div className="reaction_icon">
+                        <AiFillHeart />
+                        <AiFillHeart />
+                        <AiFillHeart />
+                        <AiFillHeart />
+                        <AiFillHeart />
+                        <AiFillHeart />
+                    </div>
+                    <div className='right icon' onClick={() => {
+                        curindex()
+                    }}><HiArrowRight /></div>
                 </div>
                 <div className="SideBar">
-                    <h1>Talent Spotlight</h1>
-                    <div></div>
-                    <h1>All Spotlights</h1>
-                        <div>
-                            {SideProfile.map((i , index)=>{
-                                return(
-                                    <div key={index}>
-                                        <h1>i.name</h1>
+                    <h2>Talent Spotlight</h2>
+                    <div className='sidebar_btn'><button><FiPlus /> Add spotlight</button></div>
+                    <h2>All Spotlights</h2>
+                    <div>
+                        {data.map((i, index) => {
+                            return (
+                                <button key={index} onClick={() => {
+                                    setCurIndex(index)
+                                }} className='storieBtn'>
+                                    <div className='StoriePerson'>
+                                        <img src={i.profile} alt={i.name} />
+                                        <div>
+                                            <h4>{i.name}</h4>
+                                            <p>{i.time} hour ago</p>
+                                        </div>
                                     </div>
-                                )
-                            })}
-                        </div>
+                                </button>
+                            )
+                        })}
+                    </div>
                 </div>
             </section>
         </>
@@ -55,119 +90,142 @@ export default function InstaStories() {
 }
 
 
-export const stories2 = [
-    {
-        url: 'https://picsum.photos/1080/1920',
-        seeMore: <SeeMore />,
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 5h ago',
-            profileImage: 'https://picsum.photos/1000/1000'
-        }
-    },
-    {
-        url:
-            'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN',
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 32m ago',
-            profileImage: 'https://picsum.photos/1080/1920'
-        }
-    },
-    {
-        url:
-            'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg',
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 32m ago',
-            profileImage:
-                'https://avatars0.githubusercontent.com/u/24852829?s=400&v=4'
-        }
-    },
-    {
-        url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4',
-        type: 'video',
-        duration: 1000,
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 5h ago',
-            profileImage: 'https://picsum.photos/1000/1000'
-        }
-    },
-    {
-        url:
-            'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-        type: 'video',
-        seeMore: <SeeMore />,
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 5h ago',
-            profileImage: 'https://picsum.photos/1000/1000'
-        }
-    },
-    {
-        url:
-            'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-        type: 'video',
-        header: {
-            heading: 'Mohit Karekar',
-            subheading: 'Posted 5h ago',
-            profileImage: 'https://picsum.photos/1000/1000'
-        }
-    },
-];
 
-export const SideProfile = [
+export const data = [
     {
         id: 1,
         name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
+        profile: 'https://cdn-icons-png.flaticon.com/512/190/190610.png',
+        time: 1,
+        visible: true,
+        item: [
+            {
+                url:
+                    'https://images.unsplash.com/photo-1590880449155-b54f958ce314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1129&q=80',
+                header: {
+                    heading: 'Kamal Karekar One 1',
+                    subheading: 'Posted 32m ago',
+                    profileImage: 'https://cdn-icons-png.flaticon.com/512/190/190610.png'
+                }
+            },
+            {
+                url: 'https://images.unsplash.com/photo-1617004446721-f712c5897941?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+
+                header: {
+                    heading: 'Kamal Karekar One 1',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://cdn-icons-png.flaticon.com/512/190/190610.png'
+                }
+            }
+
+        ]
     },
     {
         id: 2,
         name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
+        profile: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?t=st=1655793287~exp=1655793887~hmac=2befd1e6fa3e4d3a3a5d364a9d946badfdde33632c82042c2e63e6cc762a913b&w=826',
+        time: 1,
+        visible: true,
+        item: [
+            {
+                url:
+                    'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg',
+                header: {
+                    heading: 'Delta Arun Two 2',
+                    subheading: 'Posted 32m ago',
+                    profileImage:
+                        'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?t=st=1655793287~exp=1655793887~hmac=2befd1e6fa3e4d3a3a5d364a9d946badfdde33632c82042c2e63e6cc762a913b&w=826'
+                }
+            },
+            {
+                url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4',
+                type: 'video',
+                duration: 1000,
+                header: {
+                    heading: 'Delta Arun Two 2',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?t=st=1655793287~exp=1655793887~hmac=2befd1e6fa3e4d3a3a5d364a9d946badfdde33632c82042c2e63e6cc762a913b&w=826'
+                }
+            },
+            {
+                url:
+                    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+                type: 'video',
+                seeMore: <SeeMore />,
+                header: {
+                    heading: 'Delta Arun Two 2',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?t=st=1655793287~exp=1655793887~hmac=2befd1e6fa3e4d3a3a5d364a9d946badfdde33632c82042c2e63e6cc762a913b&w=826'
+                }
+            },
+            {
+                url:
+                    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+                type: 'video',
+                header: {
+                    heading: 'Delta Arun Two 2',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?t=st=1655793287~exp=1655793887~hmac=2befd1e6fa3e4d3a3a5d364a9d946badfdde33632c82042c2e63e6cc762a913b&w=826'
+                }
+            }
+        ]
     },
-
     {
         id: 3,
         name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
+        profile: 'https://cdn.vectorstock.com/i/1000x1000/14/70/monkey-graffiti-artist-vector-37071470.webp',
+        time: 1,
+        visible: true,
+        item: [
+            {
+                url: 'https://images.pexels.com/photos/4151965/pexels-photo-4151965.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+                seeMore: <SeeMore />,
+                header: {
+                    heading: 'Skyrocket Three 3',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://cdn.vectorstock.com/i/1000x1000/14/70/monkey-graffiti-artist-vector-37071470.webp'
+                }
+            },
+            {
+                url:
+                    'https://images.pexels.com/photos/12294354/pexels-photo-12294354.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+                header: {
+                    heading: 'Skyrocket Three 3',
+                    subheading: 'Posted 32m ago',
+                    profileImage: 'https://cdn.vectorstock.com/i/1000x1000/14/70/monkey-graffiti-artist-vector-37071470.webp'
+                }
+            },
+
+        ]
     },
     {
         id: 4,
         name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
-    },
-    {
-        id: 5,
-        name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
-    },
-    {
-        id: 6,
-        name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
-    },
-    {
-        id: 7,
-        name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
-    },
-    {
-        id: 8,
-        name: 'Jessica Alabama',
-        profile: 'https://picsum.photos/1080/1920',
-        time: 1
-    },
-]
+        profile: 'https://cdn.vectorstock.com/i/1000x1000/80/36/girl-in-a-mask-vector-22488036.webp',
+        time: 1,
+        visible: true,
+        item: [
+            {
+                url: 'https://images.pexels.com/photos/12339648/pexels-photo-12339648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                seeMore: <SeeMore />,
+                header: {
+                    heading: 'Mohit Karekar Four 4',
+                    subheading: 'Posted 5h ago',
+                    profileImage: 'https://cdn.vectorstock.com/i/1000x1000/80/36/girl-in-a-mask-vector-22488036.webp'
+                }
+            },
+            {
+                url:
+                    'https://images.pexels.com/photos/10429028/pexels-photo-10429028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                header: {
+                    heading: 'Mohit Karekar Four 4',
+                    subheading: 'Posted 32m ago',
+                    profileImage: 'https://cdn.vectorstock.com/i/1000x1000/80/36/girl-in-a-mask-vector-22488036.webp'
+                }
+            }
+        ]
+    }
+];
 function SeeMore() {
     return <div>see more</div>;
 }
